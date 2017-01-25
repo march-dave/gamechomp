97fc760152cd3455ce7e8f0a8d12e6e05dfa36be
march-dave <ysangy@gmail.com>
2017-01-24 19:20:33 -0500

    button add

--- index.ios.js
+++ index.ios.js
@@ -6,48 +6,46 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    Button,
+    Alert,
+    View
 } from 'react-native';
 
 export default class gamechomp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor() {
+      super();
+
+      
+    }
+
+    cleanGrid() {
+      Alert.alert('Click');
+    }
+
+    render() {
+        return ( 
+          <View style = { styles.container } >
+              
+            <Button title='click' onPress={this.cleanGrid.bind(this)} />
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+   
 });
 
 AppRegistry.registerComponent('gamechomp', () => gamechomp);
